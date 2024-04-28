7422fe4caed59128e6e1f60fd5fcd5485580d26a
mdbicode <mdbahrulilmi@gmail.com>
2024-04-28 21:52:34 +0700

    add image

--- project_flutter_ui/lib/main.dart
+++ project_flutter_ui/lib/main.dart
@@ -77,6 +77,16 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             child: Row(
               children: [
+                SizedBox(
+                  width: 180,
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                    ),
+                    child: Image.asset('assets/images/doraemon.jpeg'),
+                  ),
+                ),
                 Column(
                   children: [
                     const Icon(
