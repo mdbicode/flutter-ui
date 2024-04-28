76b353d8a23b947bc28b8dbd0e2c7f388e7ff2b2
mdbicode <mdbahrulilmi@gmail.com>
2024-04-28 20:35:18 +0700

    body

--- project_flutter_ui/lib/main.dart
+++ project_flutter_ui/lib/main.dart
@@ -49,6 +49,20 @@ class _MyHomePageState extends State<MyHomePage> {
             icon: const Icon(Icons.more_vert)),
         ],
       ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              'NAMAKU ILMI',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
